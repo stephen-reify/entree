45067f7442352d30b45e0211b0c369680449b97f
Stephen Caudill <stephen.caudill@reifyhealth.com>
2021-10-28 11:50:57 -0600

    First version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const brandColor: string = "#428";
 
-function App() {
+type MenuItem = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+};
+
+const menu: MenuItem[] = [
+  {
+    id: 1,
+    name: "Fried Chicken",
+    description: "delicious fried stuff",
+    price: 1000,
+  },
+  {
+    id: 2,
+    name: "Fried Okra",
+    description: "delicious fried stuff",
+    price: 800,
+  },
+];
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{ color: brandColor, paddingBottom: "1.2rem" }}>Entree</h1>
+      <ul>
+        {menu.map((item) => {
+          return <li key={item.id}>{item.name}</li>;
+        })}
+      </ul>
+    </>
   );
 }
-
-export default App;
