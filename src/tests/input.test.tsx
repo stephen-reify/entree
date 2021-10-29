551ebfa26831563f82c29a5dda906fea3f329a65
Stephen Caudill <stephen.caudill@reifyhealth.com>
2021-10-29 12:49:03 -0600

    Add some tests

---
+++ src/tests/input.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import { Input } from "../Input";
+
+describe("Input", () => {
+  it('should render an input type="text" and an attached label when type=text', () => {
+    render(
+      <Input
+        type="text"
+        label="label"
+        value=""
+        id=""
+        name=""
+        onChange={() => {}}
+      />
+    );
+    const input = screen.getByLabelText("label");
+    expect(input).toHaveAttribute("type", "text");
+  });
+  it("should render a textarea and an attached label when type=textarea", () => {
+    render(
+      <Input
+        type="textarea"
+        label="label"
+        value=""
+        id=""
+        name=""
+        onChange={() => {}}
+      />
+    );
+    const textarea = screen.getByLabelText("label");
+    expect(textarea).toBeInstanceOf(HTMLTextAreaElement);
+  });
+});
