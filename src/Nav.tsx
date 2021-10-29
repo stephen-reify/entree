96943df2a2f08bdb9f248a260e51da94c43fd874
Stephen Caudill <stephen.caudill@reifyhealth.com>
2021-10-29 10:08:54 -0600

    Add Nav

---
+++ src/Nav.tsx
@@ -0,0 +1,15 @@
+export function Nav() {
+  return (
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <a href="/admin">Admin</a>
+      </li>
+    </ul>
+  );
+}
