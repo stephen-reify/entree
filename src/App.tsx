18e2e8c460667dfc6ec2b307e48a35a1054b4661
Stephen Caudill <stephen.caudill@reifyhealth.com>
2021-11-01 14:00:12 -0600

    Commented out custom hook usage

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,20 @@ import { About } from "./About";
 import { Admin } from "./Admin";
 import { Nav } from "./Nav";
 import { ErrorBoundary } from "react-error-boundary";
+import { useWindowWidth } from "./hooks/useWindowWidth";
 
 export function App() {
+  const windowWidth = useWindowWidth();
   return (
     <>
+      {/*Window width: {windowWidth}*/}
       <Nav />
       <Route path="/" exact>
         <Home />
       </Route>
-
       <Route path="/about">
         <About />
       </Route>
-
       <Route path="/admin">
         <ErrorBoundary fallback={<>Sorry, the save failed. 🤦‍♀️</>}>
           <Admin />
